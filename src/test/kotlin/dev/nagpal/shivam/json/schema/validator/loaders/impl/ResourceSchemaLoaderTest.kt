1179f64095c4903c501d36247d9da2d97519ea40
Shivam Nagpal <shivamnagpal432@gmail.com>
2022-08-01 03:22:05 +0530

    Complete the branch coverage for the Loader

--- src/test/kotlin/dev/nagpal/shivam/json/schema/validator/loaders/impl/ResourceSchemaLoaderTest.kt
+++ src/test/kotlin/dev/nagpal/shivam/json/schema/validator/loaders/impl/ResourceSchemaLoaderTest.kt
@@ -5,6 +5,9 @@ import dev.nagpal.shivam.json.schema.validator.exceptions.ValidationException
 import dev.nagpal.shivam.json.schema.validator.loaders.SchemaLoader
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.io.File
+
+private const val s1 = "schema"
 
 internal class ResourceSchemaLoaderTest {
     @Test
@@ -15,7 +18,7 @@ internal class ResourceSchemaLoaderTest {
     }
 
     @Test
-    fun fetchSchemaByIdAbsentTest() {
+    fun fetchSchemaByIdAbsentTest1() {
         val schemaLoader: SchemaLoader = ResourceSchemaLoader()
         val id = "id2"
         val validationException = Assertions.assertThrowsExactly(ValidationException::class.java) {
@@ -24,6 +27,35 @@ internal class ResourceSchemaLoaderTest {
         val responseDetails = ResponseDetails.COULD_NOT_LOAD_RESOURCE_SCHEMA
         Assertions.assertEquals(responseDetails.messageCode, validationException.messageCode)
         Assertions.assertEquals(responseDetails.getMessage(id), validationException.message)
+    }
+
+    @Test
+    fun fetchSchemaByIdAbsentTest2() {
+        val pathPrefix = "schema"
+        val schemaLoader: SchemaLoader = ResourceSchemaLoader(pathPrefix = pathPrefix)
+        val id = "id3"
+        val validationException = Assertions.assertThrowsExactly(ValidationException::class.java) {
+            schemaLoader.loads(id)
+        }
+        val responseDetails = ResponseDetails.COULD_NOT_LOAD_RESOURCE_SCHEMA
+        Assertions.assertEquals(responseDetails.messageCode, validationException.messageCode)
+        Assertions.assertEquals(
+            responseDetails.getMessage("$pathPrefix${File.separator}$id"),
+            validationException.message
+        )
+    }
+
+    @Test
+    fun fetchSchemaByIdAbsentTest3() {
+        val fileNameSuffix = ".json"
+        val schemaLoader: SchemaLoader = ResourceSchemaLoader(fileNameSuffix = fileNameSuffix)
+        val id = "id2"
+        val validationException = Assertions.assertThrowsExactly(ValidationException::class.java) {
+            schemaLoader.loads(id)
+        }
+        val responseDetails = ResponseDetails.COULD_NOT_LOAD_RESOURCE_SCHEMA
+        Assertions.assertEquals(responseDetails.messageCode, validationException.messageCode)
+        Assertions.assertEquals(responseDetails.getMessage("$id$fileNameSuffix"), validationException.message)
     }
 
 }
